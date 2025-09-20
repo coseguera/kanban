ffb0ee9ae453c3bc2366d548f836d1f9a7b10bee
Carlos Oseguera <coseguera@users.noreply.github.com>
2025-09-20 00:19:56 -0700

    Remove hardcoded clientId from MSAL configuration

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 const msalConfig = {
     auth: {
-        clientId: '8b1cd1bb-5380-4314-a831-c9981a240464',
+        clientId: '',
         authority: 'https://login.microsoftonline.com/common',
         redirectUri: window.location.origin
     }
